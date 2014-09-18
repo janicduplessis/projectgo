2fda7456722c2a8ed8824ede535a773aa8b75042
Janic <janicduplessis@gmail.com>
2014-09-17 20:59:46 -0400

    more style move index -> ct-app

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+(function() {
+	ct = ct || {};
+	ct.utils = ct.utils || {};
+	ct.utils.scrollMainContentTop = function(){
+		document.querySelector('#mainContainer').scroller.scrollTop = 0;
+	}
+})();
