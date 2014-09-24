722342368c8ca26ecbb63252a81291b04b9f98fe
Janic <janicduplessis@gmail.com>
2014-09-23 22:03:39 -0400

    sql is all handled in code

--- sql/ct/tables/user.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `user` (
-  `UserId` int(11) NOT NULL AUTO_INCREMENT,
-  `UserName` varchar(45) NOT NULL,
-  `PasswordHash` varchar(64) NOT NULL,
-  `FirstName` varchar(45) NOT NULL,
-  `LastName` varchar(45) NOT NULL,
-  `Email` varchar(45) NOT NULL,
-  PRIMARY KEY (`UserId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
